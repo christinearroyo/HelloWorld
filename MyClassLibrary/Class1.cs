a1a86ce926a22ec633fd29723972d363210fcc82
Christine Arroyo <chrstinearroyo@gmail.com>
2025-01-14 19:39:50 +0800

    update

---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace MyClassLibrary
+{
+    public class Class1
+    {
+        public string ScrapeWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
